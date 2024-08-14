18926450eba8d751736db941b78740d8006788a4
Jackelyn FDZ <2130173@upv.edu.mx>
2024-08-14 07:26:04 -0600

    Revert "consultorio avance"
    This reverts commit 9ff6a50ac978fccb24aaa9bfedbf68ac188e7445.

--- resources/views/consultas/new.blade.php
+++ resources/views/consultas/new.blade.php
@@ -97,32 +97,32 @@
                         <div class="grid gap-6 mb-6 md:grid-cols-3">
                             <div>
                                 <label for="talla" class="block mb-2 text-sm font-medium text-gray-900">Talla (cm)</label>
-                                <input type="number" id="talla" name="talla" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" required/>
+                                <input type="number" id="talla" name="talla" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" />
                             </div>
                             <div>
                                 <label for="temperatura" class="block mb-2 text-sm font-medium text-gray-900">Temperatura (c°)</label>
-                                <input type="number" id="temperatura" name="temperatura" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" required/>
+                                <input type="number" id="temperatura" name="temperatura" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" />
                             </div>
                             <div>
                                 <label for="sat_oxigeno" class="block mb-2 text-sm font-medium text-gray-900">Saturación de oxígeno</label>
-                                <input type="number" id="sat_oxigeno" name="sat_oxigeno" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" required />
+                                <input type="number" id="sat_oxigeno" name="sat_oxigeno" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" />
                             </div>
                             <div>
                                 <label for="frecuencia" class="block mb-2 text-sm font-medium text-gray-900">Frecuencia cardiaca</label>
-                                <input type="number" id="frecuencia" name="frecuencia" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" required/>
+                                <input type="number" id="frecuencia" name="frecuencia" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" />
                             </div>
                             <div>
                                 <label for="peso" class="block mb-2 text-sm font-medium text-gray-900">Peso (kg)</label>
-                                <input type="number" id="peso" name="peso" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" required/>
+                                <input type="number" id="peso" name="peso" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" />
                             </div>
                             <div>
                                 <label for="tension_arterial" class="block mb-2 text-sm font-medium text-gray-900">Tensión arterial</label>
-                                <input type="number" id="tension_arterial" name="tension_arterial" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" required/>
+                                <input type="number" id="tension_arterial" name="tension_arterial" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" max="999" min="0" step="0.01" oninput="validateInput(this)" placeholder="" />
                             </div>
                         </div>
-                        <br><br><br>
+                        <br><br>
 
-                        <h1>Consulta</h1>
+                        <h2>Consulta</h2>
                         <hr style="border: 1px solid #1;">
 
                         <div class="grid gap-6 mb-6 md:grid-cols-2">
@@ -142,40 +142,23 @@
                             </div>
                         </div>
 
-                        <br><br>
-                        <h4>Receta</h4> 
                         <hr style="border: 1px solid #1;">
 
-                        <label for="medicamento" class="block mb-2 font-medium text-gray-900"><b>Productos utilizados:</b></label>
-
-                        <div class="grid gap-6 mb-6 md:grid-cols-1">
-                        <a id="boton-agregar" style="width: 150px; font-size: 12px; padding: 10px 0px; display: inline-block; background-color: black; color: white; text-decoration: none; font-weight: bold; border-radius: 5px; text-align: center;">Agregar medicamento</a>
-                    </div>
-                        
-                        <div id="medicamento-div" class="grid gap-6 mb-6 md:grid-cols-3" style="display: none;">
-                            <select id="medicamento" name="medicamento" style="height: 45px;" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                            <option value="" selected>Ninguno</option>
-                                @foreach ($productos as $producto)
-                                    <option value="{{ $producto->id }}">{{ $producto->nombre }}</option>
-                                @endforeach
-                            </select>
-
-                            <select id="frecuencia_medicamento" name="frecuencia_medicamento" style="height: 45px;" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                                <option value="" selected>Ninguno</option>
-                                <option value="2">Cada 2 hrs</option>
-                                <option value="4">Cada 4 hrs</option>
-                                <option value="8">Cada 8 hrs</option>
-                                <option value="12">Cada 12 hrs</option>
-                                <option value="24">Cada 24 hrs</option>
-                            </select>
-
-                            <div class="grid gap-6 mb-6 md:grid-cols-2">
-                                <a href="#" id="boton-agregar-otro" style="width: 150px; font-size: 12px; padding: 10px 0px; display: inline-block; background-color: black; color: white; text-decoration: none; font-weight: bold; border-radius: 5px; text-align: center;">Agregar otro</a>
-                                <a href="#" id="boton-quitar" style="width: 150px; font-size: 12px; padding: 10px 0px; display: inline-block; background-color: black; color: white; text-decoration: none; font-weight: bold; border-radius: 5px; text-align: center;">Quitar</a>
-                            </div>
+                        <label for="medicamento" class="block mb-2 font-medium text-gray-900"><b>Selecciona los productos utilizados:</b></label>
+                        <div class="grid gap-6 mb-6 md:grid-cols-4">
+                            @foreach ($productos as $producto)
+                                <label><input type="checkbox" name="medicamentos" value="{{ $producto->id }}" class="mb-2 text-sm font-medium text-gray-900"> {{ $producto->nombre }}</label>
+                            @endforeach
                         </div>
 
-                        <hr style="border: 1px solid #1;"><br>
+                        <label for="medicamento" class="block mb-2 font-medium text-gray-900"><b>Frecuencia de los productos seleccionados:</b></label>
+                        <div class="grid gap-6 mb-6 md:grid-cols-4">
+                            <label><input type="checkbox" name="medicamentos" value="2" class="mb-2 text-sm font-medium text-gray-900"> Cada 2 hrs</label>
+                            <label><input type="checkbox" name="medicamentos" value="4" class="mb-2 text-sm font-medium text-gray-900"> Cada 4 hrs</label>
+                            <label><input type="checkbox" name="medicamentos" value="8" class="mb-2 text-sm font-medium text-gray-900"> Cada 8 hrs</label>
+                            <label><input type="checkbox" name="medicamentos" value="12" class="mb-2 text-sm font-medium text-gray-900"> Cada 12 hrs</label>
+                            <label><input type="checkbox" name="medicamentos" value="24" class="mb-2 text-sm font-medium text-gray-900"> Cada 24 hrs</label>
+                        </div>
 
                         <label for="servicio" class="block mb-2 font-medium text-gray-900"><b>Selecciona los servicios requeridos para la siguiente consulta:</b></label>
                         <div class="grid gap-6 mb-6 md:grid-cols-4">
@@ -186,14 +169,9 @@
                         
 
 
-                        <div class="grid gap-6 mb-6 md:grid-cols-4">
-                        <!-- receta en pdf o consulta en pdf--><br>
-                        <a id="boton" >Imprimir Receta</a>
-                        <a href="#" id="boton">Imprimir consulta</a>
-                        </div>
-
+                        <!-- receta en pdf o consulta en pdf-->
                         <br><br>
-                        <a href="{{ route('pagos') }}" id="boton" style="width: 105px">Terminar</a>
+                        <a href="{{ route('pagos') }}" id="boton" style="width: 105px">Confirmar</a>
                         <a href="{{ route('agenda') }}" id="boton" style="width: 105px">Cancelar</a>
                         
                     </form>
@@ -212,52 +190,6 @@
                 input.value = value.slice(0, -1);
             }
         }
-
-        document.getElementById('boton-agregar').addEventListener('click', function(event) {
-            event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-            var div = document.getElementById('medicamento-div');
-            if (div.style.display === 'none') {
-                div.style.display = 'grid'; // Muestra el div
-            }
-        });
-
-        document.getElementById('boton-quitar').addEventListener('click', function(event) {
-            event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-            var div = document.getElementById('medicamento-div');
-            div.style.display = 'none'; // Oculta el div
-        });
-
-        document.getElementById('boton-agregar-otro').addEventListener('click', function(event) {
-            event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-            addMedicationSelect();
-        });
-
-        function addMedicationSelect() {
-            var container = document.getElementById('medicamentos-container');
-
-            var newSelect = document.createElement('div');
-            newSelect.classList.add('medicamento-select');
-
-            newSelect.innerHTML = `
-                <select name="medicamento[]" style="height: 45px;" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                    @foreach ($productos as $producto)
-                        <option value="{{ $producto->id }}">{{ $producto->nombre }}</option>
-                    @endforeach
-                </select>
-
-                <select name="frecuencia_medicamento[]" style="height: 45px;" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                    <option value="2">Cada 2 hrs</option>
-                    <option value="4">Cada 4 hrs</option>
-                    <option value="8">Cada 8 hrs</option>
-                    <option value="12">Cada 12 hrs</option>
-                    <option value="24">Cada 24 hrs</option>
-                </select>
-            `;
-
-            container.appendChild(newSelect);
-        }
-
-
     </script>
 
 </x-app-layout>
